bd98f7b6657c4057f0d5bd393391720bc8908aff
JaydenZheng447 <jaydenzheng447@gmail.com>
2026-03-04 14:02:32 -0500

    Update Vote.vue

--- app/src/views/Vote.vue
+++ app/src/views/Vote.vue
@@ -15,39 +15,44 @@
     </span>
     <span>
       Ethnic Minority Approval: <span>{{ Ethnic_love }}%</span>
-    </span></div>
+    </span>
+  </div>
   <div class="container">
     <h1 class="marginer">Sordland 1954 Constitution</h1>
     <img @click="increment" src="" alt="" />
     <img @click="preview" src="" />
     <img: src="" />
-        <h1 class = "marginer">Amendments</h1>
+    <h1 class="marginer">Amendments</h1>
     <h2>Amendment 1: The Supreme Court</h2>
     <p></p>
-      
+
     <h2>Amendment 2: Term Limits</h2>
     <div class="subContainer">
-          <button>&#8592</button><p>{{ s2 }}</p>
-    <button>&#8594</button>
+      <button @click="s2_arrow('left')">&#8592;</button>
+      <p>{{ s2 }}</p>
+      <button @click="s2_arrow('right')">&#8594;</button>
     </div>
+    ''
     <h2>Amendment 3: Cabinet Positions</h2>
     <div class="subContainer">
-          <button>&#8592</button><p>{{ s3 }}</p>
-    <button>&#8594</button>
-  </div>
-        
-    <h2>Amendment 4: Veto Limitations</h2>
-<div class="subContainer">
-    <button>&#8592</button>  <p>{{ s4 }}</p>
-    <button>&#8594</button>
-     </div>
-      <RouterLink to="/test-view">Test Views</RouterLink>
-  </div>
+      <button>&#8592;</button>
+      <p>{{ s3 }}</p>
+      <button>&#8594;</button>
+    </div>
 
+    <h2>Amendment 4: Veto Limitations</h2>
+    <div class="subContainer">
+      <button>&#8592;</button>
+      <p>{{ s4 }}</p>
+      <button>&#8594;</button>
+    </div>
+    <RouterLink to="/test-view">Test Views</RouterLink>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { reactive } from 'vue'
 
 let Conservative_love = ref(50)
 let Liberal_love = ref(50)
@@ -55,21 +60,156 @@ let Nationalist_love = ref(50)
 let Democratic_Socialism_love = ref(50)
 let Ethnic_love = ref(50)
 let Current_arrow = ref(1)
-let s1 = ""
-let s2 = ""
-let s3 = ""
-let s4 = ""
-s2 = s2a
-s3 = s3a
-s4 = s4a
-const s2a = "No term limits"
-const s2b = "Two-term limit"
-const s3a = "Cabinet members are appointed by the President at will"
-const s3b = "Cabinet members are appointed by the President from an approved list presented by Parliament"
-const s3c = "Cabinet members are appointed by Parliament"
-const s4a = "The President may veto any bill"
-const s4b = "Presidential vetoes can be overturned by a 2/3rd majority in Parliament"
-const s4c = "The Presidential veto shall be abolished"
+
+let s1_val = 0
+let s2_val = 0
+let s3_val = 0
+let s4_val = 0
+
+let s2_array = ['No term limits', 'Two-term limit']
+let s3_array = [
+  'Cabinet members are appointed by the President at will',
+  'Cabinet members are appointed by the President from an approved list presented by Parliament',
+  'Cabinet members are appointed by Parliament',
+]
+let s4_array = [
+  'The President may veto any bill',
+  'Presidential vetoes can be overturned by a 2/3rd majority in Parliament',
+  'The Presidential veto shall be abolished',
+]
+
+let s1 = ''
+let s2 = s2_array[s2_val]
+let s3 = s3_array[s3_val]
+let s4 = s4_array[s4_val]
+
+function s2_arrow(input) {
+  console.log(input)
+  if (input === 'left') {
+    if (s2_val > 0) {
+      s2_val -= 1
+      s2 = s2_array[s2_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else if (input === 'right') {
+    if (s2_val < s2_array.size) {
+      s2_val += 1
+      s2 = s2_array[s2_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else {
+    s2 = 'An unexpected error has occured'
+    console.log('An unexpected error has occured on Amendment 2')
+  }
+}
+
+function s3_arrow(input) {
+  if (input === 'left') {
+    if (s3_val > 0) {
+      s3_val -= 1
+      s3 = s3_array[s3_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else if (input === 'right') {
+    if (s3_val < s3_array.size) {
+      s3_val += 1
+      s3 = s3_array[s3_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else {
+    s3 = 'An unexpected error has occured'
+    console.log('An unexpected error has occured on Amendment 3')
+  }
+}
+
+function s4_arrow(input) {
+  if (input === 'left') {
+    if (s4_val > 0) {
+      s4_val -= 1
+      s4 = s4_array[s4_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else if (input === 'right') {
+    if (s4_val < s4_array.size) {
+      s4_val += 1
+      s4 = s4_array[s4_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else {
+    s4 = 'An unexpected error has occured'
+    console.log('An unexpected error has occured on Amendment 2')
+  }
+}
+
+/* 
+function s2_arrow(input) {
+  if (input === 'left') {
+    if (s2_val > 0) {
+      s2_val.value--
+      s2 = s2_array[s2_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else if (input === 'right') {
+    if (s2_val < s2_array.size) {
+      s2_val.value++
+      s2 = s2_array[s2_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else {
+    s2 = 'An unexpected error has occured'
+    console.log('An unexpected error has occured on Amendment 2')
+  }
+}
+
+function s3_arrow(input) {
+  if (input === 'left') {
+    if (s3_val > 0) {
+      s3_val.value--
+      s3 = s3_array[s3_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else if (input === 'right') {
+    if (s3_val < s3_array.size) {
+      s3_val.value++
+      s3 = s3_array[s3_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else {
+    s3 = 'An unexpected error has occured'
+    console.log('An unexpected error has occured on Amendment 3')
+  }
+}
+
+function s4_arrow(input) {
+  if (input === 'left') {
+    if (s4_val > 0) {
+      s4_val.value--
+      s4 = s4_array[s4_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else if (input === 'right') {
+    if (s4_val < s4_array.size) {
+      s4_val.value++
+      s4 = s4_array[s4_val]
+    } else {
+      console.log('Desired value out of range')
+    }
+  } else {
+    s4 = 'An unexpected error has occured'
+    console.log('An unexpected error has occured on Amendment 2')
+  }
+} */
 
 function increment() {
   count.value -= 5
@@ -80,15 +220,15 @@ function preview() {}
 </script>
 
 <style scoped>
-img {
-}
+/* img {
+} */
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 40vh;
-  max-width: 20vw;
+  /* max-width: 20vw; */
 }
 .container2 {
   display: flex;
@@ -104,18 +244,18 @@ img {
   align-items: center;
   justify-content: center;
 }
-.marginer{
+.marginer {
   margin: 10px;
 }
-.approvalBar{
+.approvalBar {
   border: 10px;
   border-color: black;
   margin: 10px;
-color:blue;
-min-width: 15 vw;
+  color: blue;
+  min-width: 15 vw;
 }
 
-button{
-  margin:5px;
+button {
+  margin: 5px;
 }
 </style>
